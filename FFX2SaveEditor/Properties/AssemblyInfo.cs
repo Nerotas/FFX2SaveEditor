84a91d11c0b73620643ceafe2e9bbe4a5cfe24b2
Nerotas <Nerotas7@gmail.com>
2025-11-09 15:24:31 -0600

    Bump version to 0.7.0.0; auto-increment produces 0.7.0.1 artifact.

--- FFX2SaveEditor/Properties/AssemblyInfo.cs
+++ FFX2SaveEditor/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-// Version 0.6
-[assembly: AssemblyVersion("0.6.0.1")]
-[assembly: AssemblyFileVersion("0.6.0.1")]
+// Version 0.7
+[assembly: AssemblyVersion("0.7.0.1")]
+[assembly: AssemblyFileVersion("0.7.0.1")]
